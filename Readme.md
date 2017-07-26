4baeb565d39182c6011037ce7136188316278a93
Etienne BESSON <etienne.besson@gmail.com>
2017-07-26 15:09:40 +0200

    intial import

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# Ansible playbook for Cozy
+Reference see :https://docs.cozy.io/en/install/manual/
---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,28 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_common_packages(host):
+    
+    assert host.package("curl").is_installed
+    assert host.package("net-tools").is_installed
+    assert host.package("nginx").is_installed
+    assert host.package("sudo").is_installed
+    assert host.package("vim-tiny").is_installed
+    assert host.package("build-essential").is_installed
+    assert host.package("pkg-config").is_installed
+    assert host.package("erlang").is_installed
+    assert host.package("libicu-dev").is_installed
+    assert host.package("libmozjs185-dev").is_installed
+    assert host.package("libcurl4-openssl-dev").is_installed
+
+
+def test_couchdb(host):
+    
+    couchdb = host.service("couchdb")
+    assert couchdb.is_running
+    assert couchdb.is_enabled
