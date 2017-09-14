9487a4b364216e38c1ef95b917f629531f962f8f
Etienne BESSON <etienne.besson@gmail.com>
2017-09-14 12:11:59 +0200

    feat(nginx): add new role so as to install and configure reverse proxy

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,9 +10,7 @@ def test_common_packages(host):
 
     assert host.package("curl").is_installed
     assert host.package("net-tools").is_installed
-    assert host.package("nginx").is_installed
     assert host.package("sudo").is_installed
-    assert host.package("vim-tiny").is_installed
     assert host.package("build-essential").is_installed
     assert host.package("pkg-config").is_installed
     assert host.package("erlang").is_installed
@@ -30,6 +28,27 @@ def test_couchdb(host):
 
 def test_cozy(host):
 
+    cozy_directories = [
+        '/var/lib/cozy',
+        '/etc/cozy'
+        ]
+    user_cozy = 'cozy'
+    group_cozy = 'cozy'
+    for cozy_directory in cozy_directories:
+        assert host.file(cozy_directory).exists
+        assert host.file(cozy_directory).is_directory
+        assert host.file(cozy_directory).user == user_cozy
+        assert host.file(cozy_directory).group == group_cozy
+
     cozy = host.service("cozy")
     assert cozy.is_running
     assert cozy.is_enabled
+
+
+def test_ngnix(host):
+
+    assert host.package("nginx").is_installed
+    nginx = host.service("nginx")
+    assert nginx.is_running
+    assert nginx.is_enabled
+    assert host.socket("tcp://0.0.0.0:443").is_listening
