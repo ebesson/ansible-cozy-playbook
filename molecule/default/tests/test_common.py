faecbb26c75ad0263bfe7a068f506ba73cfa3f41
Etienne BESSON <etienne.besson@gmail.com>
2017-10-12 22:50:16 +0200

    feat(common): use repository apt.cozy.io

--- molecule/default/tests/test_common.py
+++ molecule/default/tests/test_common.py
@@ -9,11 +9,5 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_common_packages(host):
 
     assert host.package("curl").is_installed
-    assert host.package("net-tools").is_installed
-    assert host.package("sudo").is_installed
-    assert host.package("build-essential").is_installed
-    assert host.package("pkg-config").is_installed
-    assert host.package("erlang").is_installed
-    assert host.package("libicu-dev").is_installed
-    assert host.package("libmozjs185-dev").is_installed
-    assert host.package("libcurl4-openssl-dev").is_installed
+    assert host.package("apt-transport-https").is_installed
+    assert host.package("vim").is_installed
