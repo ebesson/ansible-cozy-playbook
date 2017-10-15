27bde09012ed07c0942b72a119024dfc033830a7
Etienne BESSON <etienne.besson@gmail.com>
2017-10-15 11:32:51 +0200

    feat(cozy): use cozy-stack package

--- molecule/default/tests/test_cozy.py
+++ molecule/default/tests/test_cozy.py
@@ -6,27 +6,17 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+def test_cozy_stack_package(host):
+    assert host.package("cozy-stack").is_installed
+
+
 def test_cozy_service(host):
-    cozy = host.service("cozy")
+    cozy = host.service("cozy-stack")
     assert cozy.is_running
     assert cozy.is_enabled
 
 
-def test_cozy_directories(host):
-
-    cozy_directories = [
-        '/var/lib/cozy',
-        '/etc/cozy'
-        ]
-    user_cozy = 'cozy'
-    group_cozy = 'cozy'
-    for cozy_directory in cozy_directories:
-        assert host.file(cozy_directory).exists
-        assert host.file(cozy_directory).is_directory
-        assert host.file(cozy_directory).user == user_cozy
-        assert host.file(cozy_directory).group == group_cozy
-
-
 def test_cozy_listen_ports(host):
 
-    assert host.socket("tcp://:::8080").is_listening
+    assert host.socket("tcp://127.0.0.1:8080").is_listening
+    assert host.socket("tcp://127.0.0.1:6060").is_listening
